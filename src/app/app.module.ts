f9b198554d0755ccd3499917e63711ceeb551f46
evagolabek <golabek.eva@gmail.com>
2018-11-12 21:58:01 +0100

    app modules imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    //RouterModule.forRoot(routes),
+    ChartsModule,
     AppRoutingModule
   ],
   providers: [],
