956d6e86985d7f92814cf57cd4fad2c2526cefbf
evagolabek <golabek.eva@gmail.com>
2018-11-13 23:06:32 +0100

    line chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyBarChartComponent } from './my-bar-chart/my-bar-chart.component';
 import { MyPieChartComponent } from './my-pie-chart/my-pie-chart.component';
+import { MyLineChartComponent } from './my-line-chart/my-line-chart.component';
 
 const routes: Routes = [
   {path: 'bar-chart', component: MyBarChartComponent},
   // {path: 'doughnut-chart', component: MyDoughnutChartComponent},
-  // {path: 'radar-chart', component: MyRadarChartComponent},
+  {path: 'line-chart', component: MyLineChartComponent},
   {path: 'pie-chart', component: MyPieChartComponent},
   {path: '**', component: MyBarChartComponent }
 ];
@@ -20,7 +21,8 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     MyBarChartComponent,
-    MyPieChartComponent
+    MyPieChartComponent,
+    MyLineChartComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/my-line-chart/my-line-chart.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-my-line-chart',
+  templateUrl: './my-line-chart.component.html',
+  styleUrls: ['./my-line-chart.component.css']
+})
+export class MyLineChartComponent implements OnInit {
+
+  public lineChartData:Array<any> = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+  public lineChartLabels:Array<any> = ['January', 'February', 'March'];
+  public lineChartOptions:any = {
+    responsive: true
+  };
+  // public lineChartColors:Array<any> = [
+  //   { // grey
+  //     backgroundColor: 'rgba(255, 255, 255, 0)',
+  //     borderColor: 'rgba(148,159,177,1)',
+  //     pointBackgroundColor: 'rgba(148,159,177,1)',
+  //     pointBorderColor: '#fff',
+  //     pointHoverBackgroundColor: '#fff',
+  //     pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+  //     pointRadius: 15,
+  //   },
+  //   { // dark grey
+  //     // backgroundColor: 'rgba(255, 255, 255, 0)',
+  //     borderColor: 'rgba(77,83,96,1)',
+  //     pointBackgroundColor: 'rgba(77,83,96,1)',
+  //     pointBorderColor: '#fff',
+  //     pointHoverBackgroundColor: '#fff',
+  //     pointHoverBorderColor: 'rgba(77,83,96,1)'
+  //   }
+  // ];
+  public lineChartLegend:boolean = true;
+  public lineChartType:string = 'line';
+
+  constructor() { }
+
+  @ViewChild("myCanvas") canvas: ElementRef;
+  lineChartColors;
+
+  ngOnInit() {
+    let gradient1 = this.canvas.nativeElement.getContext('2d').createLinearGradient(0, 0, 0, 200);
+    gradient1.addColorStop(0, 'orange');
+    gradient1.addColorStop(1, 'purple');
+
+    let gradient2 = this.canvas.nativeElement.getContext('2d').createLinearGradient(0, 0, 0, 200);
+    gradient2.addColorStop(0, 'yellow');
+    gradient2.addColorStop(1, 'green');
+
+    this.lineChartColors = [
+      {
+        borderColor: gradient1,
+        backgroundColor: 'rgba(255, 255, 255, 0)',
+        // borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        pointRadius: 15,
+      },
+      {
+        borderColor: gradient2,
+        backgroundColor: 'rgba(255, 255, 255, 0)', 
+        pointBackgroundColor: 'rgba(77,83,96,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(77,83,96,1)'
+      }
+    ];
+  }
+
+}
