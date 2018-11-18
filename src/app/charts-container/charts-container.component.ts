4dc5e6933c1eddd88e59615d56de60eb0cf13f2a
evagolabek <golabek.eva@gmail.com>
2018-11-18 12:50:30 +0100

    line chart used for display data

--- src/app/charts-container/charts-container.component.ts
+++ src/app/charts-container/charts-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LineChart } from '../my-line-chart/my-line-chart.component';
 
 @Component({
   selector: 'app-charts-container',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./charts-container.component.css']
 })
 export class ChartsContainerComponent implements OnInit {
+  salesOctober: LineChart;
 
   constructor() { }
 
   ngOnInit() {
-  }
-
+    this.salesOctober ={
+      title: "Sales October",
+      infoText: "my info text",
+      lineDataPoints: [
+        {name: 'pen', value: 35},
+        {name: 'notebook', value: 28},
+        {name: 'rubber', value: 10}
+      ]
+    };
+}
 }
--- src/app/my-line-chart/my-line-chart.component.ts
+++ src/app/my-line-chart/my-line-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, ViewChild} from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Input} from '@angular/core';
 
 @Component({
   selector: 'app-my-line-chart',
@@ -6,13 +6,18 @@ import { Component, OnInit, ElementRef, ViewChild} from '@angular/core';
   styleUrls: ['./my-line-chart.component.css']
 })
 export class MyLineChartComponent implements OnInit {
+  @Input() lineChart: LineChart;
 
+  title: string;
+  lineChartLabels: string[];
+  lineChartData: number[];
+  infoText: string;
 
-  public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
-  ];
-  public lineChartLabels:Array<any> = ['January', 'February', 'March'];
+  // public lineChartData:Array<any> = [
+  //   {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+  //   {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  // ];
+  // public lineChartLabels:Array<any> = ['January', 'February', 'March'];
   public lineChartOptions:any = {
     responsive: true
   };
@@ -55,6 +60,15 @@ export class MyLineChartComponent implements OnInit {
       }
     ];
   }
+}
 
+export interface LineChart {
+  title: string;
+  infoText: string;
+  lineDataPoints: LineDataPoint[];
+}
+export interface LineDataPoint {
+  name: string;
+  value: number;
 }
 
