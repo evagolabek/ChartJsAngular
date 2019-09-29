4a51320c2299910c33b7e74d5c786afe6a526bea
evagolabek <golabek.eva@gmail.com>
2019-09-29 23:54:03 +0200

    chart to pdf

--- src/app/my-bar-chart/my-bar-chart.component.ts
+++ src/app/my-bar-chart/my-bar-chart.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit, ElementRef, ViewChild} from '@angular/core';
+// import * as jsPDF from 'jspdf';
+// import * as html2canvas from 'html2canvas';
+// const html2canvas = require('../../../node_modules/html2canvas');
+// import html2pdf from 'html2pdf.js'
+
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-my-bar-chart',
@@ -6,6 +12,8 @@ import { Component, OnInit, ElementRef, ViewChild} from '@angular/core';
   styleUrls: ['./my-bar-chart.component.css']
 })
 export class MyBarChartComponent implements OnInit {
+
+  
 
   constructor() { }
   
@@ -54,4 +62,69 @@ export class MyBarChartComponent implements OnInit {
       }
     ];
   }
+
+
+//   download() {
+//     var doc = new jsPDF();
+//     doc.text(20, 20, 'Hello world!');
+//     doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.');
+//     doc.addPage();
+//     doc.text(20, 20, 'Do you like that?');
+
+//     // Save the PDF
+//     doc.save('Test.pdf');
+// }
+
+// download() {
+//   html2canvas(this.canvas.nativeElement, <Html2Canvas.Html2CanvasOptions>{
+//     onrendered: function(canvas: HTMLCanvasElement) {
+//       var pdf = new jsPDF('p','pt','a4');    
+//       var img = canvas.toDataURL("image/png");
+//       pdf.addImage(img, 'PNG', 10, 10, 580, 300);
+//       pdf.save('web.pdf');
+//     }
+//   });
+// }
+
+// download() {
+//   var element = document.getElementById('test');
+//   var opt = {
+//     margin:       1,
+//     filename:     'myfile.pdf',
+//     image:        { type: 'jpeg', quality: 0.98 },
+//     html2canvas:  { scale: 2 },
+//     jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+//   };
+
+//   // New Promise-based usage:
+//   html2pdf().set(opt).from(element).save();
+// }
+
+download() {
+  var pdf = new jsPDF('p', 'pt', 'letter');
+  var source = document.getElementById('test');
+  var margins = {
+    top: 80,
+    bottom: 60,
+    left: 40,
+    width: 522
+  };
+  // all coords and widths are in jsPDF instance's declared units
+  // 'inches' in this case
+  pdf.fromHTML(
+      source, // HTML string or DOM elem ref.
+      margins.left, // x coord
+      margins.top, { // y coord
+          'width': margins.width, // max width of content on PDF
+      },
+
+      function (dispose) {
+          // dispose: object with X, Y of the last line add to the PDF 
+          //          this allow the insertion of new lines after html
+          pdf.save('Test.pdf');
+      }, margins
+  );
+}
+
+
 }
